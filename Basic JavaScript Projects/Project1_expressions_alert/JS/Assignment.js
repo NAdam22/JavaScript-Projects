f8c611485079bb286ddb3af8911329ef658a5c2c
Naif A <naifissa22@gmail.com>
2025-09-29 03:15:18 -0300

    Add files via upload

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/Assignment.js
@@ -0,0 +1,16 @@
+
+// Basic JavaScript Projects Assignment
+// window method for alert
+window.alert("Hello, World!");
+
+// document method to write to the HTML document
+document.write("This Basic JavaScript Assignment, hello, World!");
+
+// creating string 2 variables
+var sen1 = "This is the beginning of the string.";
+var sen2 = "and this is the end of the string.";
+// dispalying variables method
+document.write(sen1 +  sen2);
+
+// arithmetic expression
+document.write(33 * 84);
---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,30 @@
+
+// JavaScript alert
+alert("Hello, World!");
+// Window method for alert
+window.alert("Welcome to JavaScript!");
+// document method to write to the HTML document
+document.write("This is a simple expression alert example.");
+// String variable
+var A = "This is a string.";
+document.write(A);
+// Another string variable with alert
+var name = "Naif";
+window.alert(name);
+// Concatenation of strings and special characters
+document.write("Naif told Ali, \"Knock it off, Ali! or I'll tell dad!\"<br> \"Eat my shorts!\" Ali retorted.");
+document.write("\" Be who you are and say what you feel," + " because those who mind don't matter, and those who matter don't mind.\"<br> - Dr. Seuss");
+// Concatenation of string variables
+var B = "concatenated" + " string";
+document.write(B); 
+// Multiple variables in one statement
+var Family = "the Arezzinis", Dad = "Jeremiah", mom = "Hernoise", Daughter = "Penny", Son = "Zorro";
+document.write(Dad);
+// Arithmetic expression 
+3 + 3;
+document.write(3 + 3); 
+
+
+
+
+
